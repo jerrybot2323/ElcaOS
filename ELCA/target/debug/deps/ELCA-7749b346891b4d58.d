6a1c87f780daba8d9dfa47b460f58a58a17f33c1
JerryBot2323 <agindecaleb@gmail.com>
2026-04-06 03:31:30 +0000

    Added my script

---
+++ ELCA/target/debug/deps/ELCA-7749b346891b4d58.d
@@ -0,0 +1,5 @@
+/workspaces/ElcaOS/ELCA/target/debug/deps/ELCA-7749b346891b4d58.d: src/main.rs
+
+/workspaces/ElcaOS/ELCA/target/debug/deps/libELCA-7749b346891b4d58.rmeta: src/main.rs
+
+src/main.rs:
---
+++ ELCA/target/debug/deps/ELCA-d48d385c8bf0d847.d
@@ -0,0 +1,5 @@
+/workspaces/ElcaOS/ELCA/target/debug/deps/ELCA-d48d385c8bf0d847.d: src/main.rs
+
+/workspaces/ElcaOS/ELCA/target/debug/deps/libELCA-d48d385c8bf0d847.rmeta: src/main.rs
+
+src/main.rs:
